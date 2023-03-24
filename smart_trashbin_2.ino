7600c739dcf68c98d396b99a84104623d3073618
chheangg <balldremz@gmail.com>
2023-03-24 14:08:39 +0700

    Implement detection for operating servo

--- smart_trashbin_2.ino
+++ smart_trashbin_2.ino
@@ -1,4 +1,3 @@
-
 // Include Libraries
 #include "Arduino.h"
 #include "NewPing.h"
@@ -6,23 +5,22 @@
 
 
 // Pin Definitions
-#define HCSR04_PIN_TRIG	3
-#define HCSR04_PIN_ECHO	2
-#define SERVO360MICRO_PIN_SIG	4
+#define HCSR04_PIN_TRIG  3
+#define HCSR04_PIN_ECHO  2
+#define SERVO9G_PIN_SIG  4
 
 
 
 // Global variables and defines
-
+const int servo9gRestPosition   = 0;  //Starting position
+const int servo9gTargetPosition = 90 //Position when event is detected
 // object initialization
 NewPing hcsr04(HCSR04_PIN_TRIG,HCSR04_PIN_ECHO);
-Servo servo360Micro;
+Servo servo9g;
 
 
 // define vars for testing menu
 const int timeout = 10000;       //define timeout of 10 sec
-char menuOption = 0;
-long time0;
 
 // Setup the essentials for your circuit to work. It runs first every time your circuit is powered with electricity.
 void setup() 
@@ -30,78 +28,30 @@ void setup()
     // Setup Serial which is useful for debugging
     // Use the Serial Monitor to view printed messages
     Serial.begin(9600);
-    while (!Serial) ; // wait for serial port to connect. Needed for native USB
-    Serial.println("start");
-    
-    
-    menuOption = menu();
-    
+    servo9g.attach(SERVO9G_PIN_SIG);
 }
 
-// Main logic of your circuit. It defines the interaction between the components you selected. After setup, it runs over and over again, in an eternal loop.
-void loop() 
+void loop()
 {
-    
-    
-    if(menuOption == '1') {
-    // Ultrasonic Sensor - HC-SR04 - Test Code
-    // Read distance measurment from UltraSonic sensor           
-    int hcsr04Dist = hcsr04.ping_cm();
-    delay(10);
-    Serial.print(F("Distance: ")); Serial.print(hcsr04Dist); Serial.println(F("[cm]"));
+  hcListener(hcsr04.ping_cm());
+  delay(60); 
+}
 
-    }
-    else if(menuOption == '2') {
-    // Continuous Rotation Micro Servo - FS90R - Test Code
-    // The servo will rotate CW in full speed, CCW in full speed, and will stop  with an interval of 2000 milliseconds (2 seconds) 
-    servo360Micro.attach(SERVO360MICRO_PIN_SIG);         // 1. attach the servo to correct pin to control it.
-    servo360Micro.write(180);  // 2. turns servo CW in full speed. change the value in the brackets (180) to change the speed. As these numbers move closer to 90, the servo will move slower in that direction.
-    delay(2000);                              // 3. waits 2000 milliseconds (2 sec). change the value in the brackets (2000) for a longer or shorter delay in milliseconds.
-    servo360Micro.write(0);    // 4. turns servo CCW in full speed. change the value in the brackets (0) to change the speed. As these numbers move closer to 90, the servo will move slower in that direction.
-    delay(2000);                              // 5. waits 2000 milliseconds (2 sec). change the value in the brackets (2000) for a longer or shorter delay in milliseconds.
-    servo360Micro.write(90);    // 6. sending 90 stops the servo 
-    delay(2000);                              // 7. waits 2000 milliseconds (2 sec). change the value in the brackets (2000) for a longer or shorter delay in milliseconds.
-    servo360Micro.detach();                    // 8. release the servo to conserve power. When detached the servo will NOT hold it's position under stress.
-    }
-    
-    if (millis() - time0 > timeout)
-    {
-        menuOption = menu();
-    }
-    
+void hcListen(int distance)
+{
+  // distance in cm
+  if (distance < 2)
+  {
+    rotateServo(servo9gTargetPosition);
+  } else {
+    rotateServo(servo9gRestPosition);
+  }
 }
 
 
-
-// Menu function for selecting the components to be tested
-// Follow serial monitor for instrcutions
-char menu()
+void rotateServo(int deg)
 {
-
-    Serial.println(F("\nWhich component would you like to test?"));
-    Serial.println(F("(1) Ultrasonic Sensor - HC-SR04"));
-    Serial.println(F("(2) Continuous Rotation Micro Servo - FS90R"));
-    Serial.println(F("(menu) send anything else or press on board reset button\n"));
-    while (!Serial.available());
-
-    // Read data from serial monitor if received
-    while (Serial.available()) 
-    {
-        char c = Serial.read();
-        if (isAlphaNumeric(c)) 
-        {   
-            
-            if(c == '1') 
-    			Serial.println(F("Now Testing Ultrasonic Sensor - HC-SR04"));
-    		else if(c == '2') 
-    			Serial.println(F("Now Testing Continuous Rotation Micro Servo - FS90R"));
-            else
-            {
-                Serial.println(F("illegal input!"));
-                return 0;
-            }
-            time0 = millis();
-            return c;
-        }
-    }
+  servo9g.write(deg);
+  // duration for disposal
+  delay(1000);  
 }
